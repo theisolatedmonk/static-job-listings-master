742c0a86ce9ed43421f2591422c6d8f3a747e4c9
theisolatedmonk <96rahulseth@gmail.com>
2023-10-26 13:56:20 +0530

    conditional border created

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,43 +1,48 @@
-import React from 'react'
+'use client'
+// import React, { useState } from 'react'
 import Image from 'next/image'
-import data from 'data.json'
+import data from '@/app/data.json'
 type Props = {
     data: string
 }
 
 export default function Card({ }: Props) {
+    // const [classes, setClasses] = useState(false)
     return (
         <>
-            {data.map((data) => (<div key={data.id} className='flex justify-between sm:flex-row flex-col gap-4 sm:p-4 sm:px-8 p-4 px-6 bg-lightGrayishCyanBackground rounded-md shadow-2xl'>
-                <div className="flex gap-4 sm:flex-row flex-col sm:items-center ">
-                    <Image src={data.logo} alt={''} width={80} height={80} />
+            <div className="flex flex-col gap-4">
+                {data.map((data) => (<div key={data.id} className={`flex justify-between sm:flex-row flex-col gap-4 sm:p-4 sm:px-8 p-4 px-6 bg-lightGrayishCyanBackground rounded-md shadow-md  ${data.new && data.featured ? 'border-l-[4px] border-l-desaturatedDarkCyan' : ''}`}>
+                    <div className={`flex gap-4 sm:flex-row flex-col sm:items-center `}>
+                        <Image src={data.logo} alt={''} width={80} height={80} />
 
-                    <div className="flex flex-col gap-2">
-                        <div className="flex gap-2 font-semibold">
-                            <p className="text-desaturatedDarkCyan ">{data.company}</p>
-                            <p className="bg-desaturatedDarkCyan p-1 rounded-3xl px-2 text-lightGrayishCyanBackground">NEW!</p>
-                            <p className="bg-veryDarkGrayishCyan p-1 rounded-3xl px-2 text-lightGrayishCyanBackground">FEATURED</p>
-                        </div>
-                        <p className="font-bold text-veryDarkGrayishCyan ">{data.position}</p>
-                        <div className="flex  justify-start gap-4 text-sm  items-center text-darkGrayishCyan">
+                        <div className="flex flex-col gap-2">
+                            <div className="flex gap-2 font-semibold">
+                                <p className="text-desaturatedDarkCyan ">{data.company}</p>
+                                <p className={`bg-desaturatedDarkCyan p-1 rounded-3xl px-2 text-lightGrayishCyanBackground ${data.new ? 'flex' : 'hidden'}`}>NEW!</p>
+                                <p className={`bg-veryDarkGrayishCyan p-1 rounded-3xl px-2 text-lightGrayishCyanBackground  ${data.featured ? 'flex' : 'hidden'}`}>FEATURED</p>
+                            </div>
+                            <p className="font-bold text-veryDarkGrayishCyan cursor-pointer">{data.position}</p>
+                            <div className="flex  justify-start gap-4 text-sm  items-center text-darkGrayishCyan">
 
-                            <p className="">{data.postedAt}</p>
-                            <p className="bg-darkGrayishCyan h-1 w-1 rounded-full"></p>
-                            <p className="">{data.contract}</p>
-                            <p className="bg-darkGrayishCyan h-1 w-1 rounded-full"></p>
-                            <p className="">{data.location}</p>
+                                <p className="">{data.postedAt}</p>
+                                <p className="bg-darkGrayishCyan h-1 w-1 rounded-full"></p>
+                                <p className="">{data.contract}</p>
+                                <p className="bg-darkGrayishCyan h-1 w-1 rounded-full"></p>
+                                <p className="">{data.location}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <hr className="sm:hidden w-full  " />
-                <div className="flex gap-2 sm:items-center sm:justify-end justify-start sm:flex-row flex-wrap ">
+                    <hr className="sm:hidden w-full  " />
+                    <div className="flex gap-2 sm:items-center sm:justify-end justify-start sm:flex-row flex-wrap ">
 
-                    <p className="hover:bg-desaturatedDarkCyan bg-lightGrayishCyanFilterTablets rounded-lg p-2 text-desaturatedDarkCyan font-semibold hover:text-lightGrayishCyanBackground">{data.role}</p>
-                    <p className="hover:bg-desaturatedDarkCyan bg-lightGrayishCyanFilterTablets rounded-lg p-2 text-desaturatedDarkCyan font-semibold hover:text-lightGrayishCyanBackground">{data.level}</p>
-                    <p className="hover:bg-desaturatedDarkCyan bg-lightGrayishCyanFilterTablets rounded-lg p-2 text-desaturatedDarkCyan font-semibold hover:text-lightGrayishCyanBackground">{data.languages}</p>
-                    <p className="hover:bg-desaturatedDarkCyan bg-lightGrayishCyanFilterTablets rounded-lg p-2 text-desaturatedDarkCyan font-semibold hover:text-lightGrayishCyanBackground">{data.tools}</p>
-                </div>
-            </div>))}
+                        <p className="hover:bg-desaturatedDarkCyan bg-lightGrayishCyanFilterTablets rounded-lg p-2 text-desaturatedDarkCyan font-semibold hover:text-lightGrayishCyanBackground cursor-pointer">{data.role}</p>
+                        <p className="hover:bg-desaturatedDarkCyan bg-lightGrayishCyanFilterTablets rounded-lg p-2 text-desaturatedDarkCyan font-semibold hover:text-lightGrayishCyanBackground cursor-pointer">{data.level}</p>
+                        {data.languages.map((d, i) => <p key={i} className="hover:bg-desaturatedDarkCyan bg-lightGrayishCyanFilterTablets rounded-lg p-2 text-desaturatedDarkCyan font-semibold hover:text-lightGrayishCyanBackground cursor-pointer">{d} </p>)}
+
+                        <p className="hover:bg-desaturatedDarkCyan bg-lightGrayishCyanFilterTablets rounded-lg p-2 text-desaturatedDarkCyan font-semibold hover:text-lightGrayishCyanBackground cursor-pointer">{data.tools}</p>
+                    </div>
+                </div>))}
+            </div>
         </>
 
     )
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -4,9 +4,9 @@ type Props = {}
 
 export default function Searchbar({ }: Props) {
     return (
-        <div className="flex justify-end w-full  bg-white rounded-md items-center font-semibold h-14 shadow-lg p-4 gap-2 outline-none border-none focus:outline-none">
-            <input type="text" className='w-full rounded-md p-2 flex flex-wrap' />
-            <p className=" rounded-md">Clear</p>
+        <div className="flex justify-end w-full  bg-white rounded-md items-center font-semibold h-14 shadow-lg p-4 gap-2 outline-none border-none group group-focus-within:ring-1">
+            <input type="text" className='w-full rounded-md p-2 flex flex-wrap outline-none' />
+            <button className=" rounded-md cursor-pointer">Clear</button>
         </div>
     )
 }
